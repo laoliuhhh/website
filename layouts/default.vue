e31ee7b545a24bd477be0602cd38e70949158249
sekaom <dulin081682@126.com>
2024-04-28 02:41:25 +0800

    Init commit:just a page now

---
+++ layouts/default.vue
@@ -0,0 +1,165 @@
+<template>
+
+    <div>
+        <header class="z-20 bg-background/80 backdrop-blur">
+            <UiContainer class="flex h-16 items-center justify-between lg:h-20">
+                <div class="flex items-center gap-10">
+                    <NuxtLink to="#" class="flex items-center gap-3">
+                        <!-- eslint-disable-next-line vue/html-self-closing -->
+                        <img src="https://resource.neokoni.moe/craft233/img/craft233_logo.svg" fit="contain"
+                            alt="Company Logo" title="Company Logo" class="h-6 object-contain lg:h-8" />
+                        <!-- <span class="font-semibold lg:text-lg">Craft233</span> -->
+                    </NuxtLink>
+                    <UiNavigationMenu as="nav" class="hidden items-center justify-start gap-8 lg:flex">
+                        <UiNavigationMenuList class="gap-2">
+                            <UiNavigationMenuItem>
+                                <UiNavigationMenuLink as-child>
+                                    <UiButton to="#" variant="ghost" size="sm">主页</UiButton>
+                                </UiNavigationMenuLink>
+                            </UiNavigationMenuItem>
+                            <UiNavigationMenuItem>
+                                <UiNavigationMenuLink as-child>
+                                    <a href="https://docs.craft233.top" target="_blank"><UiButton variant="ghost"size="sm">文档
+                                        <Icon name="lucide:external-link" class="h-4 w-4"/>
+                                    </UiButton></a>
+                                    
+                                </UiNavigationMenuLink>
+                            </UiNavigationMenuItem>
+                            <UiNavigationMenuItem>
+                                <UiNavigationMenuLink as-child>
+                                    <UiButton to="#" variant="ghost" size="sm">团队</UiButton>
+                                </UiNavigationMenuLink>
+                            </UiNavigationMenuItem>
+                        </UiNavigationMenuList>
+                    </UiNavigationMenu>
+                </div>
+                <div class="lg:hidden">
+                    <UiSheet>
+                        <UiSheetTrigger as-child>
+                            <UiButton variant="ghost" size="icon-sm">
+                                <Icon name="lucide:menu" class="h-5 w-5" />
+                            </UiButton>
+                            <UiSheetContent class="w-[90%] p-0">
+                                <template #content>
+                                    <UiSheetTitle class="sr-only" title="Mobile menu" />
+                                    <UiSheetDescription class="sr-only" description="Mobile menu" />
+                                    <UiSheetX class="z-20" />
+
+                                    <UiScrollArea class="h-full p-5">
+                                        <div class="flex flex-col gap-2">
+                                            <UiButton variant="ghost" class="justify-start text-base" to="#">主页
+                                            </UiButton>
+                                            <UiButton variant="ghost" class="justify-start text-base" to="#">文档
+                                            </UiButton>
+                                            <UiButton variant="ghost" class="justify-start text-base" to="#">团队
+                                            </UiButton>
+                                        </div>
+                                    </UiScrollArea>
+                                </template>
+                            </UiSheetContent>
+                        </UiSheetTrigger>
+                    </UiSheet>
+                </div>
+            </UiContainer>
+        </header>
+        <slot />
+        <UiContainer as="footer" class="py-16 ">
+            <section class="grid grid-cols-2 gap-8 lg:grid-cols-6">
+                <div class="col-span-full pb-10 lg:col-span-2">
+                    <NuxtLink to="#" class="flex items-center gap-3">
+                        <!-- eslint-disable-next-line vue/html-self-closing -->
+                        <img src="https://resource.neokoni.moe/craft233/img/craft233_logo.svg" fit="contain"
+                            alt="Company Logo" title="Company Logo" class="h-7 object-contain lg:h-10" />
+                        <span class="text-xl font-semibold">Craft233</span>
+                    </NuxtLink>
+                    <p class="mt-5 text-muted-foreground">
+                        该写些什么呢？
+                    </p>
+                </div>
+                <template v-for="n in 1">
+                    <div>
+                        <p class="mb-4 text-sm text-muted-foreground">交流</p>
+                        <ul class="flex flex-col gap-3">
+                            <li>
+                                <NuxtLink target="_blank" class="font-semibold hover:text-primary"
+                                    to="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=QofGtNhyZdlU0kVZtr81sGxVDMzKGkkt&authKey=59dPS936wuYm%2Bg4qHFkMdo8IYTcdD6Jp8Q9kBb2Mmv8i7APEm0Ms2373ztiW97wE&noverify=0&group_code=830277457">
+                                    QQ群
+                                    <Icon name="lucide:external-link" class="h-4 w-4"/>
+                                </NuxtLink>
+                            </li>
+                        </ul>
+                    </div>
+                </template>
+                <template v-for="n in 1">
+                    <div>
+                        <p class="mb-4 text-sm text-muted-foreground">Craft233</p>
+                        <ul class="flex flex-col gap-3">
+                            <li>
+                                <NuxtLink class="font-semibold hover:text-primary" target="_blank"
+                                    to="https://github.com/Craft233MC">Github
+                                <Icon name="lucide:external-link" class="h-4 w-4"/></NuxtLink>
+                            </li>
+                            <li>
+                                <NuxtLink class="font-semibold hover:text-primary" target="_blank"
+                                    to="https://space.bilibili.com/3546655496342120">BiliBili
+                                    <Icon name="lucide:external-link" class="h-4 w-4"/></NuxtLink>
+                            </li>
+                        </ul>
+                    </div>
+                </template>
+                <template v-for="n in 1">
+                    <div>
+                        <p class="mb-4 text-sm text-muted-foreground">宣传贴</p>
+                        <ul class="flex flex-col gap-3">
+                            <li>
+                                <NuxtLink class="font-semibold hover:text-primary" target="_blank"
+                                    to="https://www.minebbs.com/threads/1-13-x-1-20-x-craft233-2.19401/">MineBBS
+                                    <Icon name="lucide:external-link" class="h-4 w-4"/>
+                                </NuxtLink>
+                            </li>
+                            <li>
+                                <NuxtLink class="font-semibold hover:text-primary" target="_blank" to="#">苦力怕论坛
+                                    <Icon name="lucide:external-link" class="h-4 w-4"/>
+                                </NuxtLink>
+                            </li>
+                        </ul>
+                    </div>
+                </template>
+                <template v-for="n in 1">
+                    <div>
+                        <p class="mb-4 text-sm text-muted-foreground">相关链接</p>
+                        <ul class="flex flex-col gap-3">
+                            <li>
+                                <NuxtLink class="font-semibold hover:text-primary" target="_blank"
+                                    to="https://skin.craft233.top/">Craft233 Skin
+                                    <Icon name="lucide:external-link" class="h-4 w-4"/></NuxtLink>
+                            </li>
+                            <li>
+                                <NuxtLink class="font-semibold hover:text-primary" target="_blank"
+                                    to="https://status.craft233.top/status/craft233">外部监测站
+                                    <Icon name="lucide:external-link" class="h-4 w-4"/></NuxtLink>
+                            </li>
+                            <li>
+                                <NuxtLink class="font-semibold hover:text-primary" target="_blank"
+                                    to="https://afdian.net/a/craft233/">爱发电
+                                    <Icon name="lucide:external-link" class="h-4 w-4"/></NuxtLink>
+                            </li>
+                        </ul>
+                    </div>
+                </template>
+            </section>
+
+            <section class="mt-12 flex flex-col justify-between gap-5 border-t pt-8 lg:mt-16 lg:flex-row">
+                <p class="text-muted-foreground">
+                    &copy; 2023-{{ new Date().getFullYear() }} Craft233. All rights reserved.
+                </p>
+                <div class="flex items-center gap-2">
+                    <Icon name="lucide:github" class="h-7 w-7" />
+                    <Icon name="ri:bilibili-line" class="h-7 w-7"/>
+                </div>
+            </section>
+        </UiContainer>
+
+
+    </div>
+</template>
---
+++ pages/1.vue
@@ -0,0 +1,56 @@
+<template>
+  <UiContainer class="py-16 lg:py-24">
+    <h2 class="mb-4 mt-3 text-center text-3xl font-semibold lg:mb-5 lg:text-4xl">
+      我们的特色
+    </h2>
+    <!-- <p class="mx-auto max-w-[760px] text-center text-lg text-muted-foreground lg:text-xl">
+      Powerful, self-serve product and growth analytics to help you convert, engage, and retain more
+      users. Trusted by over 4,000 startups.
+    </p> -->
+
+    <div
+      class="grid grid-cols-1 gap-y-10 py-10 md:grid-cols-2 lg:grid-cols-3 lg:gap-8 lg:gap-y-16 lg:py-16"
+    >
+      <template v-for="(f1, i1) in features1" :key="i1">
+        <div class="group flex flex-col items-center justify-center">
+          <div class="flex h-12 w-12 items-center justify-center rounded-md border">
+            <Icon
+              :name="f1.icon"
+              class="h-5 w-5 transition-colors group-hover:text-primary lg:h-6 lg:w-6"
+            />
+          </div>
+          <h3
+            class="mt-4 text-balance text-center text-lg font-semibold lg:mt-5 lg:text-xl"
+            v-html="f1.title"
+          />
+          <p
+            class="mt-1 max-w-[400px] text-balance text-center text-muted-foreground lg:mt-2"
+            v-html="f1.description"
+          />
+        </div>
+      </template>
+    </div>
+  </UiContainer>
+</template>
+
+<script lang="ts" setup>
+  const features1 = [
+    {
+      icon: "lucide:cable",
+      title: "多版本兼任",
+      description:
+        "支持Java版1.13及之后的版本以及基岩版的最新正式版",
+    },
+    {
+      icon: "lucide:shield-check",
+      title: "数据安全",
+      description:
+        "每日凌晨自动备份数据，保证数据安全",
+    },
+    {
+      icon: "lucide:circle-arrow-up",
+      title: "版本新",
+      description: `当插件或模组支持，我们会追随最新的Minecraft版本`,
+    },
+  ];
+</script>
