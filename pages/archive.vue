9e0ac0cbd2df256baa37dc4403712d66f9c59b96
neokoni <dulin081682@126.com>
2024-08-03 04:05:56 +0800

    fix: safari display error

--- pages/archive.vue
+++ pages/archive.vue
@@ -11,40 +11,47 @@
                     </div>
                 </div>
             </slot>
-            <div class="lg:py-24 py-16"></div>
-
-            <div>
-                <div class="flex flex-col gap-5 md:flex-row">
-                    <NuxtLink
-                        :to="'https:/' + '/' + 'pan.neokoni.moe/%E9%98%BF%E9%87%8C%E4%BA%91%E7%9B%98%20%7C%20AliDrive/Craft233/%E5%AD%98%E6%A1%A3/week1/backup-2024-02-23(2).zip'"
-                        class="shrink-0" target="_blank">
-                        <!-- eslint-disable-next-line vue/html-self-closing -->
-                        <img src="/assets/img/week1/2023-08-31_00.03.12.png" alt="第一周目主城"
-                            class="h-[240px] w-full rounded-lg object-cover shadow md:h-[200px]" />
-                    </NuxtLink>
-                    <div>
+            <div class="lg:py-20 py-16"></div>
+            <template v-for="(h, i) in history" :key="i">
+                <section
+                    class="mt-12 grid grid-cols-1 items-center gap-10 lg:h-[280px] lg:grid-cols-2 lg:gap-20">
+                    <div :class="[i % 2 == 0 ? 'lg:order-none' : 'lg:order-1']">
                         <NuxtLink
-                            :to="'https:/' + '/' + 'pan.neokoni.moe/%E9%98%BF%E9%87%8C%E4%BA%91%E7%9B%98%20%7C%20AliDrive/Craft233/%E5%AD%98%E6%A1%A3/week1/backup-2024-02-23(2).zip'"
+                            :to="h.downloadUrl"
                             target="_blank">
-                            <p class="mb-2 text-xl font-semibold lg:text-2xl">第一周目</p>
+                        <h3 class="mb-2 text-2xl font-semibold lg:mb-4 lg:text-3xl" v-html="h.title" />
                         </NuxtLink>
-                        <p class="mb-5 line-clamp-2 text-ellipsis text-muted-foreground">
-                            Craft233最原始的一个周目，此时我们正在用不知名的配置开服，一切都是未知的，每一次修改都是对开服的学习
-                        </p>
-                        <div class="items-center">
-                            <div>
-                                <p class="text-sm text-muted-foreground">2023.7.10--2024.2.23</p>
-                            </div>
-                        </div>
+                        <p class="text-muted-foreground lg:text-lg mb-5" v-html="h.description" />
+                        <p class="text-muted-foreground lg:text-lg" v-html="h.data"></p>
                     </div>
-                </div>
-            </div>
+                    <!-- eslint-disable-next-line vue/html-self-closing -->
+                    <NuxtLink
+                        :to="h.downloadUrl"
+                        class="shrink-0" target="_blank">
+                    <img :src="h.imageUrl" :alt="h.title"
+                        class="h-[180px] w-full rounded-lg object-cover shadow-sm lg:h-[320px]" />
+                    </NuxtLink>
+                </section>
+            </template>
 
         </UiContainer>
     </NuxtLayout>
 </template>
 
-<script>
+<script lang="ts" setup>
+const history = [
+    {
+        title: "第一周目",
+        description:
+            "Craft233最原始的一个周目，此时我们正在用不知名的配置开服，一切都是未知的，每一次修改都是对开服的学习",
+        imageUrl:
+            "/assets/img/week1/2023-08-31_00.03.12.png",
+        data: "2023.7.10--2024.2.23",
+        downloadUrl: "https://pan.neokoni.moe/%E9%98%BF%E9%87%8C%E4%BA%91%E7%9B%98%20%7C%20AliDrive/Craft233/%E5%AD%98%E6%A1%A3/week1/backup-2024-02-23(2).zip",
+    },
+];
+
+
 // 页面标题
 const runtimeConfig = useRuntimeConfig()
 useHead({
