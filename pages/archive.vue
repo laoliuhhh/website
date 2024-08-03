8bbd902f24b17ea8a97fdd2fa15c290ff95185f3
neokoni <dulin081682@126.com>
2024-08-04 02:11:45 +0800

    fix : archive data

--- pages/archive.vue
+++ pages/archive.vue
@@ -46,7 +46,7 @@ const history = [
             "Craft233最原始的一个周目，此时我们正在用不知名的配置开服，一切都是未知的，每一次修改都是对开服的学习",
         imageUrl:
             "/assets/img/week1/2023-08-31_00.03.12.png",
-        data: "2024.7.10--2024.8.3",
+        data: "2023.7.10--2024.2.24",
         downloadUrl: "https://pan.neokoni.moe/%E9%98%BF%E9%87%8C%E4%BA%91%E7%9B%98%20%7C%20AliDrive/Craft233/%E5%AD%98%E6%A1%A3/week1/backup-2024-02-23(2).zip",
     },
     {
@@ -55,7 +55,7 @@ const history = [
             "第二周目的生电服存档，因无人感兴趣加入与管理维护降低，在很长之间之前就已是空服。第二周目并未结束，仅Cube",
         imageUrl:
             "/assets/img/week2/cube/cube-screenshot-from-bili.png",
-        data: "2024.3.1--2024.2.23",
+        data: "2024.3.10--2024.8.3",
         downloadUrl: "https://pan.neokoni.moe/%E9%98%BF%E9%87%8C%E4%BA%91%E7%9B%98%20%7C%20AliDrive/Craft233/%E5%AD%98%E6%A1%A3/week2/cube/cube-backup-2024-08-03-22-32.tar.xz",
     },
 ];
